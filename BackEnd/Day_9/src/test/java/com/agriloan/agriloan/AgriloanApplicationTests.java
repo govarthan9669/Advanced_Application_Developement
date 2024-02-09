266b9276346dd7c77db254bb7ba234856e9e64d6
GOVARTHAN R T <114054480+govarthan9669@users.noreply.github.com>
2024-02-09 09:39:58 +0530

    Change

---
+++ BackEnd/Day_9/src/test/java/com/agriloan/agriloan/AgriloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriloan.agriloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
